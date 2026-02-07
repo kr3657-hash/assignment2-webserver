568b5f67683af06e20fdb36559f71713e64b2b0d
kr3657-hash <kr3657@nyu.edu>
2026-02-07 09:42:01 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -12,22 +12,23 @@ def webServer(port=13331):
   serverSocket.bind(("", port))
   
   #Fill in start
-
+  #Listening for a connection
+  serverSocket.listen(1) 
   #Fill in end
 
   while True:
     #Establish the connection
     
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
+    connectionSocket, addr = serverSocket.accept() #Fill in start -are you accepting connections?     #Fill in end
     
     try:
-      message = #Fill in start -a client is sending you a message   #Fill in end 
+      message = connectionSocket.recv(1024).decode() #Fill in start -a client is sending you a message   #Fill in end 
       filename = message.split()[1]
       
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:],     #fill in start              #fill in end   )
+      f = open(filename[1:], 'rb')     #fill in start              #fill in end   )
       
       
 
@@ -36,6 +37,10 @@ def webServer(port=13331):
               
       #Content-Type is an example on how to send a header as bytes. There are more!
       outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata = b"HTTP/1.1 200 OK\r\n"
+      outputdata += b"Server: MyPythonWebServer/1.0\r\n"
+      outputdata += b"Connection: close\r\n"
+      outputdata += b"\r\n"
 
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
@@ -43,11 +48,11 @@ def webServer(port=13331):
       #Fill in end
                
       for i in f: #for line in file
-      #Fill in start - append your html file contents #Fill in end 
+        outputdata += i
         
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
-
+      connectionSocket.send(outputdata)
       # Fill in start
 
 
@@ -58,15 +63,16 @@ def webServer(port=13331):
     except Exception as e:
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
-      #Fill in start
-
-      #Fill in end
-
+      outtputdata = b"HTTP/1.1 404 Not Found\r\n"
+      outputdata += b"Server: MyPythonWebServer/1.0\r\n"
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata += b"Connection: close\r\n"
+      outputdata += b"\r\n"
+      outputdata += b"<html><body><h1>404 Not Found</h1></body></html>"
+      connectionSocket.send(outputdata)
 
       #Close client socket
-      #Fill in start
-
-      #Fill in end
+      connectionSocket.close()
 
   # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop. 
   # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
@@ -75,3 +81,4 @@ def webServer(port=13331):
 
 if __name__ == "__main__":
   webServer(13331)
+
